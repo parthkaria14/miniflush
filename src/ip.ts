7fc6b4ff76eb20820ad5215bd090c35c29fba969
kktheomniscient <kk.the.omniscient@gmail.com>
2025-08-16 11:01:07 +0530

    ip changed

--- src/ip.ts
+++ src/ip.ts
@@ -1,4 +1,4 @@
-// export const IP = "192.168.2.190"; //casino's ethernet
+export const IP = "192.168.2.190"; //casino's ethernet
 // export const IP = "192.168.1.4"; //kk's laptop
-export const IP = "192.168.31.60"; //pk's laptop
+// export const IP = "192.168.31.60"; //pk's laptop
 // export const IP = "192.168.1.39"; //pk-office laptop
