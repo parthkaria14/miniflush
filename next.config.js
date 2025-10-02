68da0d3c22df837f6618e1b2c709ab1fcfb3bf3d
kktheomniscient <kk.the.omniscient@gmail.com>
2025-10-02 09:14:38 +0530

    changes1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  // Disable Next.js floating dev overlay + build activity spinner
+  experimental: {
+    disableNextDevOverlay: true,
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
